64e207d98030edb5ade55a3e5a97eaf0ea69e19f
Quan Xu <xuquan69220@gmail.com>
2022-09-09 20:44:55 +0200

    Add emailRow

---
+++ src/EmailRow.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./EmailRow.css";
+import { IconButton } from "@mui/material";
+import Checkbox from "@mui/material/Checkbox";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+
+function EmailRow({ id, title, subject, description, time }) {
+  return (
+    <div className="emailRow">
+      <div className="emailRow__options">
+        <Checkbox />
+        <IconButton>
+          <StarBorderIcon />
+        </IconButton>
+        <IconButton>
+          <LabelImportantIcon />
+        </IconButton>
+      </div>
+
+      <h3 className="emailRow__title">{title}</h3>
+
+      <div className="emailRow__message">
+        <h4>
+          {subject}
+          <span className="emailRow__description">-{description}</span>
+        </h4>
+      </div>
+
+      <p className="emailRow__time">{time}</p>
+    </div>
+  );
+}
+
+export default EmailRow;
